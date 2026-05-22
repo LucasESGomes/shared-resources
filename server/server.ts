ff363a782c63091be03ee3bbeb7ee1088d4cdff1
Lucas Gomes <158865689+LucasESGomes@users.noreply.github.com>
2026-05-22 14:09:46 -0300

    Start server on port 5000
    Added server start call to initiate the Fastify app.

--- server/server.ts
+++ server/server.ts
@@ -7,6 +7,7 @@ const app = fastify;
 
 const PORT = Number(process.env.PORT || 5000);
 
+// Chamando servidor na porta 5000
 const start = async () => {
     try {
         await app.listen({ port: PORT, host: '0.0.0.0' });
